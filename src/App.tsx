4718f6fe24dc2450ce68abf58b9edd8bb5576e41
atorri <alessandro.torri@gmail.com>
2023-03-31 17:15:27 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import axios from "axios";
+import Driver from "./components/driver.component";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [drivers, setDrivers] = useState([]);
+	const getDrivers = async () => {
+		try {
+			const response = await axios.get("data/patenti.json");
+
+			setDrivers(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	useEffect(() => {
+		getDrivers();
+	}, []);
+
+	return (
+		<div className="App">
+			{drivers ? (
+				Object.values(drivers).map((driver, index) => {
+					return <Driver key={index} driver={driver} />;
+				})
+			) : (
+				<div style={{ color: "white", backgroundColor: "black" }}>
+					NOT AN ARRAY
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
